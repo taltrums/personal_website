dd7c2fd4abe74acfb599dbe0064f78b94301fcb4
sidecodes <sidekicktal@outlook.com>
2020-12-20 20:14:16 +0530

    Added str function for descriptive objects

--- projects/models.py
+++ projects/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Project(models.Model):
     image = models.ImageField(upload_to='images/')
     summary = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.summary
