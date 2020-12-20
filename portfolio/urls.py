b32ba394d54d8342202a73fd4a4d285d9153b53b
sidecodes <sidekicktal@outlook.com>
2020-12-20 20:16:38 +0530

    Added url pattern for detail

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -21,7 +21,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', projects.views.home, name='home')
+    path('', projects.views.home, name='home'),
+    path('projects/<int:project_id>', projects.views.detail, name='detail'),
 ] 
 
 urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
